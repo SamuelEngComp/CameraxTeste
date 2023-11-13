e7b8701eaecb17bf0181e0bee4d500b0b5350922
SamuelEngComp <samuellima810@gmail.com>
2023-11-13 10:40:59 -0300

    first

---
+++ app/src/main/java/br/com/engcomp/camerax/screen/MainScreen.kt
@@ -0,0 +1,22 @@
+package br.com.engcomp.camerax.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun MainScreen(){
+    Greeting("Samuel")
+}
+
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
